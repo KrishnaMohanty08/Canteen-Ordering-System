26655cf3a3b4d0350acd50381933ec01f067445f
Sessobin <mohantykrishna57@gmail.com>
2025-05-09 21:01:45 +0530

    user

--- JAVA/User.java
+++ JAVA/User.java
@@ -1,21 +1,19 @@
-import java.io.*;
 import java.sql.*;
+import java.util.Scanner;
 
 class sql {
     private static final String url = "jdbc:mysql://localhost:3306/canteen_system";
     private static final String root = "root";
     private static final String password = "MohantY#08";
 
-    // Method to get a database connection
     public Connection getConnection() throws SQLException {
         try {
-            // Load the MySQL JDBC driver
+
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             throw new SQLException("MySQL JDBC Driver not found", e);
         }
-        // Return the connection using the defined credentials
         return DriverManager.getConnection(url, root, password);
     }
 
@@ -38,15 +36,68 @@ class sql {
             throw e;
         }
     }
+
+    public void delete(int prod_id) throws SQLException {
+        Scanner sc=new Scanner(System.in);
+        String query = "delete from Menu where prod_id=?";
+
+        try (Connection con = getConnection();
+                PreparedStatement Pstmt = con.prepareStatement(query)){
+                    // System.out.println("Enter the Index of the row ");
+                    // int id=sc.nextInt();
+                    // System.out.println("Enter the Columnn Name:");
+                    // String colName=sc.next();
+                    Pstmt.setInt(1,prod_id);
+                    int rowsAffected=Pstmt.executeUpdate();
+                    System.out.println(rowsAffected +"deleted");
+                            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+    
+    public void update(int prod_id) throws SQLException {
+        Scanner sc=new Scanner(System.in);
+        String query = "update from Menu where prod_id=?";
+
+        try (Connection con = getConnection();
+                PreparedStatement Pstmt = con.prepareStatement(query)){
+                    // System.out.println("Enter the Index of the row ");
+                    // int id=sc.nextInt();
+                    // System.out.println("Enter the Columnn Name:");
+                    // String colName=sc.next();
+                    Pstmt.setInt(1,prod_id);
+                    int rowsAffected=Pstmt.executeUpdate();
+                    System.out.println(rowsAffected +"deleted");
+                            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
 }
 
 public class User {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
         sql data = new sql();
-        try {
-            data.readAll();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Enter the number for the operation :\n 1:read all data \n 2:delete one");
+        int n=in.nextInt();
+        switch(n){
+            case 1:{
+                try{
+                    data.readAll();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }case 2:{
+                try{
+                    data.delete(1);
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            
+        }        
     }
 }
