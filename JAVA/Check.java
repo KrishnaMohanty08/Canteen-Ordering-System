9f9bb671ce7c077a3e4430d73fcc882c4c366534
Nipun Kumar Ranjan <nkr.062005@gmail.com>
2025-05-15 11:39:18 +0530

    For checking the presence of Admin

---
+++ JAVA/Check.java
@@ -0,0 +1,12 @@
+public class Check 
+{
+    static String Admin[]={"1234","2341","5678","8901"};
+    static String password[]={"abc","bcd","def","fgh"};
+    static String username[]={"admin1","admin2","admin3","admin4"};
+
+    public String check(String us,String pass)
+    {
+        
+        return "false";
+    }
+}
